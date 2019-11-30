e015369439c78e5aa0a42883d12573f2fc0997ed
Stanton Viaduc <sjviaduc@gmail.com>
2019-11-30 13:29:11 -0700

    add server

---
+++ server/src/test/java/com/stantonviaduc/springbootvue/SpringBootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stantonviaduc.springbootvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
